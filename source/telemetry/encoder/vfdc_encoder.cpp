813fe39883219f75898618080b0bea6258cc17ed
Tijunel <justintijunel@gmail.com>
2022-01-07 21:19:36 -0700

    Basic vfdcp working

--- source/telemetry/encoder/vfdc_encoder.cpp
+++ source/telemetry/encoder/vfdc_encoder.cpp
@@ -2,3 +2,79 @@
 Copyright Schulich Racing, FSAE
 Written by Justin Tijunelis
 */ 
+
+#include "vfdc_encoder.h"
+
+CompressedData VFDCPEncoder::encode_data(std::vector<SensorVariantPair>& data) {
+  // Sort the variants from largest to smallest with respect to bytes.
+  std::sort(data.begin(), data.end(),
+    [](SensorVariantPair &a, SensorVariantPair &b) {
+      size_t a_size = 0; 
+      std::visit([&](auto v) { a_size = sizeof(v); }, std::get<1>(a));
+      size_t b_size = 0;
+      std::visit([&](auto v) { b_size = sizeof(v); }, std::get<1>(b));
+      return a_size > b_size;
+    });
+
+  // Determine the amount of memory we need to allocate 
+  // size = number of sensor ids + sum of data type sizes for current data + padding ([0, 3])
+  size_t size = 0;
+  size += size_t(data.size());
+  for (const SensorVariantPair &pair : data) {
+    size_t current_size;
+    std::visit([&](auto v) { current_size = sizeof(v); }, std::get<1>(pair));
+    size += current_size;
+  }
+  if (!(size % 4)) size += size % 4;
+  
+  // Append the sensor ids at the beginning of the byte array
+  char compressed_data[size];
+  char* temp = compressed_data;
+  for (const SensorVariantPair &pair : data) {
+    *temp = std::get<0>(pair);
+    temp++;
+  }
+
+  // Append all of the sensor data
+  for (const SensorVariantPair &pair : data) {
+    std::visit(
+      [&](auto v) { 
+        *temp = v;
+        temp += sizeof(v);
+      }, 
+      std::get<1>(pair)
+    );
+  }
+
+  return CompressedData{ compressed_data, size };
+}
+
+/* Unit Tests */
+void test_one_value() {
+  unsigned char id = 1;
+  SensorDataVariant datum = int(5);
+  SensorVariantPair pair = { id, datum };
+  std::vector<SensorVariantPair> data;
+  data.push_back(pair);
+  CompressedData compressed = VFDCPEncoder::get().encode_data(data);
+
+  // Ensure our size is 5
+  assert(std::get<1>(compressed) == 5);
+
+  // Ensure the first byte is the char id = 1
+  char *encoding = std::get<0>(compressed);
+  assert(encoding[0] == 1);
+  
+  // Ensure the first value is 5
+  int value = encoding[1];
+  assert(value == 5);
+}
+
+// void test_multiple_values() {
+//   std::vector<SensorVariantPair> data;
+//   data.push_back({ 1, long long()})
+// }
+
+int main() {
+  test_one_value();
+}
--- source/telemetry/encoder/vfdc_encoder.h
+++ source/telemetry/encoder/vfdc_encoder.h
@@ -4,8 +4,60 @@ Written by Justin Tijunelis
 */ 
 
 #pragma once
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <variant>
+#include <algorithm>
+#include <cassert>
 
-// Variable Frequnecy Data Protocol
-class VFDP {
+/**
+ * @brief This represents all the possible data types our system
+ *  can read. Based on delegation with the team, we found
+ *  that these datatypes can represent all of our sensors.
+ *  In the future, this list may be expanded.
+ */
+using SensorDataVariant =
+  std::variant<
+    long long, // q - char type representation from sensor
+    double,    // d
+    float,     // f
+    int,       // i
+    short,     // h
+    char,      // c
+    bool       // ?
+  >;
 
+/**
+ * @brief A tuple (sensor ID, SensorVariantData)
+ */
+using SensorVariantPair = std::tuple<unsigned char, SensorDataVariant>;
+
+/**
+ * @brief An array of bytes containing our sensor ids
+ * and compressed data along with a size for sending 
+ * sensor data of UDP. 
+ */
+using CompressedData = std::tuple<char*, size_t>; 
+
+/**
+ * @brief Variable Frequency Data Compression Protocal Encoder
+ * @details A singleton class that provides an interface for passing 
+ *  a list of variants and outputs a byte array containing
+ *  all information needed to decode the data on the server side. 
+ */
+class VFDCPEncoder {
+  private:
+    VFDCPEncoder() {}
+
+  public:
+    VFDCPEncoder(const VFDCPEncoder&) = delete;
+    VFDCPEncoder &operator=(const VFDCPEncoder&) = delete;
+
+    static VFDCPEncoder &get() {
+      static VFDCPEncoder instance;
+      return instance;
+    }
+
+    CompressedData encode_data(std::vector<SensorVariantPair>& data);
 };
