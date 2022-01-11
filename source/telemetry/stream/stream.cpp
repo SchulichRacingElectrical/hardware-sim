636e0cbbab51c9d2777fabad84eda0d9e2076e8e
Tijunel <justintijunel@gmail.com>
2022-01-10 18:40:44 -0700

    Fixed stream

--- source/telemetry/stream/stream.cpp
+++ source/telemetry/stream/stream.cpp
@@ -45,54 +45,52 @@ void Stream::_tap_channels() noexcept {
         continue;
       }
 
-      // Read from every channel, update if the value has changed significantly
+      // Determine which channels we should be reading from. 
       for (auto it = _channels.begin(); it != _channels.end(); it++) {
         AbstractChannel *abstract_channel = it->second;
         std::visit(
           [&](auto v) {
-            // Cast the channel to the right type and check if we should read from the channel at this timestamp
-            Channel<decltype(v)> *channel = dynamic_cast<Channel<decltype(v)>*>(abstract_channel);
-            unsigned int channel_frequency = channel->sensor.frequency;
+            // Determine whether the sensor value should be read on the next frequency sync
+            unsigned int channel_frequency = abstract_channel->sensor.frequency;
             float divisor = floor(1000.0f / float(channel_frequency));
-            if (std::fmod(_timestamp, divisor) > std::numeric_limits<float>::epsilon()) {
-              return;
-            }
-
-            // Update the buffer if the value has significantly changed (0.5% difference)
-            SensorRange bounds = channel->sensor.bounds;
-            int epsilon = (bounds.upper - bounds.lower) * 0.005f;
-            std::visit(
-              [&](auto last_value) {
-                auto current_value = channel->read();
-                if (current_value.has_value()) {
-                  if (abs(*current_value - last_value) > epsilon) {
-                    _stream_buffer[channel->sensor.channel_id] = *current_value;
-                    change_set.insert(it->first);
-                  }
-                }
-              }, 
-              _stream_buffer[channel->sensor.channel_id]
-            );
+            if (std::fmod(_timestamp, divisor) < std::numeric_limits<float>::epsilon()) {
+              change_set.insert(it->first);
+            } 
           }, 
           abstract_channel->sensor.get_variant()
         );
       }
 
-      // Create the data that only contains values for this timestep and have significantly changed
-      std::vector<SensorVariantPair> data;
-      for (const unsigned int& channel_id: change_set) {
-        unsigned char sensor_id = _channels[channel_id]->sensor.id;
-        auto value = _stream_buffer[channel_id];
-        data.emplace_back(SensorVariantPair{sensor_id, value});
-      }
-
-      // Notify all subscribers of a new data frame. 
+      // Perform a read on the change_set if the timestamp aligns with the highest channel frequency
       float min_divisor = floor(1000.0f / _frequency);
-      if (data.size() != 0 && std::fmod(_timestamp, min_divisor) < std::numeric_limits<float>::epsilon()) {
-        for (auto it = _callbacks.begin(); it != _callbacks.end(); it++) {
-          it->second(_timestamp, data);
+      if (std::fmod(_timestamp, min_divisor) < std::numeric_limits<float>::epsilon()) {
+        // Read from the channels and append if they have significantly change
+        std::vector<SensorVariantPair> data;
+        for (const unsigned int& channel_id: change_set) {
+          std::visit(
+            [&](auto last_value) {
+              Channel<decltype(last_value)> *channel = dynamic_cast<Channel<decltype(last_value)>*>(_channels[channel_id]);
+              auto current_value = channel->read(_timestamp);
+              SensorRange bounds = channel->sensor.bounds;
+              int epsilon = (bounds.upper - bounds.lower) * 0.005f;
+              if (current_value.has_value()) {
+                if (abs(*current_value - last_value) > epsilon) {
+                  _stream_buffer[channel->sensor.channel_id] = *current_value;
+                  data.push_back({channel->sensor.id, *current_value});
+                }
+              }
+            }, 
+            _stream_buffer[channel_id]
+          );
         }
-        change_set.clear();
+
+        // Notify the subscribers of new data
+        if (data.size() != 0) {
+          for (auto it = _callbacks.begin(); it != _callbacks.end(); it++) {
+            it->second(_timestamp, data);
+          }
+          change_set.clear();
+        }
       }
       _timestamp = _timestamp + 1;
     }
